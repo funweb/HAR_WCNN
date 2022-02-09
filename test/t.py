f258ebb1e361258fc7a9f8abc0f903e143b3d012
antOS <943164605@qq.com>
2022-02-09 15:00:11 +0800

    uodate

--- test/t.py
+++ test/t.py
@@ -1,47 +1,8 @@
-import urllib, json, os, ipykernel, ntpath
-from notebook import notebookapp as app
+period = 30
+epochs = 150
 
+for epoch in range(epochs):
+    if (period - (epoch % period) < period*0.2) and (epoch % int(period*0.2/3) == 0):
+        print(epoch)
 
-def lab_or_notebook():
-    length = len(list(app.list_running_servers()))
-    if length:
-        return "notebook"
-    else:
-        return "lab"
-
-
-def ipy_nb_name(token):
-    """ Returns the short name of the notebook w/o .ipynb
-        or get a FileNotFoundError exception if it cannot be determined
-        NOTE: works only when the security is token-based or there is also no password
-    """
-
-    if lab_or_notebook() == "lab":
-        from jupyter_server import serverapp as app
-    else:
-        from notebook import notebookapp as app
-    #         from jupyter_server import serverapp as app
-
-    connection_file = os.path.basename(ipykernel.get_connection_file())
-    kernel_id = connection_file.split('-', 1)[1].split('.')[0]
-
-    #     from notebook import notebookapp as app
-    for srv in app.list_running_servers():
-        try:
-            srv['token'] = token
-            if srv['token'] == '' and not srv['password']:  # No token and no password, ahem...
-                req = urllib.request.urlopen(srv['url'] + 'api/sessions')
-                print('no token or password')
-            else:
-                req = urllib.request.urlopen(srv['url'] + 'api/sessions?token=' + srv['token'])
-            sessions = json.load(req)
-            for sess in sessions:
-                if sess['kernel']['id'] == kernel_id:
-                    nb_path = sess['notebook']['path']
-                    return ntpath.basename(nb_path).replace('.ipynb', '')  # handles any OS
-        except:
-            pass  # There may be stale entries in the runtime directory
-    raise FileNotFoundError("Can't identify the notebook name")
-
-
-ipy_nb_name("8786ba7fd6db486eb13a6e4e79d5951a")
+print(1)
