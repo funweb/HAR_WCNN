f2368fa7331909c39575992d8492c467323c52fc
antOS <943164605@qq.com>
2022-02-19 00:34:03 +0800

    update

--- test/a.py
+++ test/a.py
@@ -1,5 +1,15 @@
-from glob import glob
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(np.arange(20).reshape(5,4),index=list("ABCDE"),columns=list("WXYZ"))
 
-for i in glob("../*ipynb"):
-    print(i)
+print(df)
+print(df.loc[:, "Y"])
+
+index_list = ["0", "1", "2", "acc_mean", "acc_std"]
+
+df_2 = pd.DataFrame(index=index_list)
+
+df_2["999"] = df.loc[:, "Y"].tolist()
+
+print(df_2)
 
