e74136cf71fa55d8b9523a22887c014cea03ee25
antOS <943164605@qq.com>
2022-02-18 16:58:42 +0800

    update

--- test/a.py
+++ test/a.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-'''
-@File    :   a.py    
-@Contact :   raogx.vip@hotmail.com
-@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+d = {
+    "i" for i in range(3)
+     }
 
-@Modify Time      @Author    @Version    @Desciption
-------------      -------    --------    -----------
-2022/2/12 16:17   gxrao      1.0         None
-'''
 
-# import lib
+d["a"].update({"aa":1})
+d["a"].update({"bb":1})
+
+print(d)
